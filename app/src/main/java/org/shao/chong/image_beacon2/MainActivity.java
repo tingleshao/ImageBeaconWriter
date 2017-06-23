8bb3e5963a7bba4373a7655c1de841d5dc44b155
tingleshao <cshao@cs.unc.edu>
2017-06-23 14:35:27 -0400

    save update

--- app/src/main/java/org/shao/chong/image_beacon2/MainActivity.java
+++ app/src/main/java/org/shao/chong/image_beacon2/MainActivity.java
@@ -2,8 +2,24 @@ package org.shao.chong.image_beacon2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.estimote.sdk.Beaconmanager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementChecker;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button leftTakeButton;
+    Button rightTakeButton;
+    Button writeButton;
+
+    private BeaconManager beaconManager;
+    private Region region;
+    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
